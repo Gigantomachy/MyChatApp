8d7b4a0e06b07a8b6392093cfe2f22fd534ab5af
Steven Chu <steven.chu.1997@gmail.com>
2026-06-07 22:02:56 -0400

    added auth middleware to prepare for routes

---
+++ backend/monolithic/internal/middleware/jwt_auth.go
@@ -0,0 +1,69 @@
+package middleware
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// our authentication middleware, will set "user_id" that can be
+// used by downstream handlers and routes
+func JWTAuthMiddleware() gin.HandlerFunc {
+	// JWTAuthMiddleware itself is only run once when we wire up the routes
+	// so env read will only happen once during startup here
+	// inner function captures the variable
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		secret = "dev-secret-change-in-production"
+	}
+
+	return func(c *gin.Context) {
+		authorization := c.GetHeader("Authorization")
+		if authorization == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "authorization header missing"})
+			return
+		}
+
+		parts := strings.SplitN(authorization, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Bearer token is missing"})
+			return
+		}
+
+		bearer := parts[1]
+
+		token, err := jwt.Parse(bearer, func(token *jwt.Token) (any, error) {
+
+			// verify that the signing method is what we expect
+			// jwt.SigningMethodHS256 is not a type - the underlying type is jwt.SigningMethodHMAC
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("unexpected signing method")
+			}
+
+			return secret, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid or expired token"})
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			userID, exists := claims["user_id"]
+			if !exists || userID == "" {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token claims"})
+				return
+			}
+
+			c.Set("user_id", userID)
+		} else {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token claims"})
+			return
+		}
+
+		c.Next()
+	}
+}
